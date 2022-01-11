c41c537b8716e0ebd128c0a4022b43c57ddd4b2a
Filippo Rossi <rossifilippo2000@gmail.com>
2022-01-11 12:44:37 +0100

    init page

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Footer'
+	}
+</script>
+
+<style scoped lang="scss">
+	@import "../style/Global.scss";
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Header'
+	}
+</script>
+
+<style scoped lang="scss">
+	@import "../style/Global.scss";
+</style>
