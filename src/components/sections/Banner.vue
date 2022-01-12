02d17dae2217dfc2d4629aad5f915bf4dc9c72b8
Filippo Rossi <rossifilippo2000@gmail.com>
2022-01-12 12:43:48 +0100

    Banner

--- src/components/sections/Banner.vue
+++ src/components/sections/Banner.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <img src="../../assets/img/ebay-logotan-2x-200x66.png" alt="Ebay Logo">
+        <img src="../../assets/img/audible-logotan-2x-200x66.png" alt="Audible Logo">
+        <img src="../../assets/img/bb-logotan-2x-200x66.png" alt="Barnes&noble Logo">
+        <img src="../../assets/img/kindlefire-logotan-2x-200x66.png" alt="Kindle fire Logo">
     </div>
 </template>
 
@@ -12,4 +15,14 @@
 
 <style lang="scss" scoped>
     @import "../../style/Global.scss";
+    .container{
+        height: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: $tan;
+        img{
+            margin: 0px 30px;
+        }
+    }
 </style>
